c9b0d2c7c51c9505c361c6441febe80761b312de
Deepak Kumar <162724035+deepakkumar7186@users.noreply.github.com>
2026-05-21 14:39:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (53.11%) - LeetSync

---
+++ 2383-add-two-integers/add-two-integers.java
@@ -0,0 +1,9 @@
+//import java.util.LongInteger;
+class Solution {
+    public int sum(int num1, int num2) {
+        //LongInteger sum = new LongInteger();
+        int sum = num1 + num2;
+
+        return sum;
+    }
+}
