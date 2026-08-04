153f8a04357933ccc25fabb353599f4ec6a3f875
Deepak Kumar <162724035+deepakkumar7186@users.noreply.github.com>
2026-08-04 22:06:41 +0530

    Time: 3 ms (81.76%) | Memory: 46.7 MB (67.71%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> missing = new ArrayList<>();
+        if (nums == null || nums.length == 0) return missing;
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+        for (int i = min; i <= max; i++) {
+            if (!set.contains(i)) missing.add(i);
+        }
+        return missing;
+    }
+}
